2f652edcd70241549aad004558b9d8a377c594c5
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-07-09 03:48:07 +0530

    setup app config

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:play_hq/helpers/networks/app_configurations.dart';
+import 'package:play_hq/helpers/networks/config.dart';
 import 'package:play_hq/playhq_head.dart';
 
 import 'helpers/app-service-locator.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   setupLocator();
+  ConfigData.setBaseUrl(Environment.DEV);
   runApp(PlayHqHead());
 }
-
