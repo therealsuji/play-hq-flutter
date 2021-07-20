cc50670697436b0b9ed217e2d5a90d19bfd0155b
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-07-20 20:00:39 +0530

    added id to model

--- lib/models/app-search-game-model.dart
+++ lib/models/app-search-game-model.dart
@@ -1,24 +1,26 @@
-class SearchGame{
-
+class SearchGame {
   List<GameDetails> _data;
 
-  SearchGame.fromJson(Map<String, dynamic> json){
-    _data = json['results'] == null ? null : List<GameDetails>.from(json['results'].map((street)=> GameDetails.fromJson(street)));
+  SearchGame.fromJson(Map<String, dynamic> json) {
+    _data = json['results'] == null
+        ? null
+        : List<GameDetails>.from(json['results'].map((street) => GameDetails.fromJson(street)));
   }
 
   List<GameDetails> get data => _data;
-
 }
 
-class GameDetails{
+class GameDetails {
   String _name;
   String _released;
   String _image;
+  String _id;
 
-  GameDetails.fromJson(Map<String, dynamic> json){
+  GameDetails.fromJson(Map<String, dynamic> json) {
     _name = json['name'] == null ? null : json['name'];
     _released = json['released'] == null ? null : json['released'];
     _image = json['background_image'] == null ? null : json['background_image'];
+    _id = json['id'] == null ? null : json['id'];
   }
 
   String get image => _image;
@@ -26,5 +28,6 @@ class GameDetails{
   String get released => _released;
 
   String get name => _name;
-}
 
+  String get id => _id;
+}
