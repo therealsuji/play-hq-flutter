85978b7b9afdd6acec86ad021654a39f88e0a276
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-07-24 19:42:22 +0530

    removed initial value

--- lib/view-models/create-sale/impl-create-sale.dart
+++ lib/view-models/create-sale/impl-create-sale.dart
@@ -2,16 +2,6 @@ import 'package:play_hq/models/app-search-game-model.dart';
 import 'package:play_hq/view-models/create-sale/create-sale-model.dart';
 
 class ImplCreateSale extends CreateSaleModel {
-  ImplCreateSale() : super() {
-    var test2 = new GameDetails(
-        id: 2,
-        name: "uncharted 2",
-        released: "no",
-        image: "https://i.pinimg.com/236x/3a/02/68/3a02685be25e504ea6bd2848e2a9715c--jeux-ps-ps-games.jpg");
-
-    gameList.addAll([test2]);
-  }
-
   @override
   addGame(GameDetails game) {
     gameList.add(game);
