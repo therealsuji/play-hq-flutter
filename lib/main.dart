2adbe0a9196f588028fc576c6b54740b80f89b9b
Malshan Perera <malshan@taloslogy.com>
2021-07-31 03:27:53 +0530

    Refactor Main and Header file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:play_hq/models/search-model/app-search-game-model.dart';
 import 'package:play_hq/playhq-head.dart';
 import 'helpers/app-service-locator.dart';
 import 'helpers/networks/app-config.dart';
 import 'helpers/networks/app-configurations.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  final appDocumentaryDirectory = await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentaryDirectory.path);
-  setupLocator();
   ConfigData.setBaseUrl(Environment.STAGE);
-  Hive.registerAdapter(GameDetailsAdapter());
+  await Firebase.initializeApp();
+  setupLocator();
   runApp(PlayHqHead());
 }
--- lib/playhq-head.dart
+++ lib/playhq-head.dart
@@ -1,57 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:play_hq/helpers/app-colors.dart';
-import 'package:play_hq/helpers/app-strings.dart';
+import 'package:play_hq/models/search-model/app-search-game-model.dart';
 import 'package:play_hq/services/nav-service.dart';
 import 'helpers/app-fonts.dart';
 import 'helpers/app-routes.dart';
 import 'helpers/app-service-locator.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-class PlayHqHead extends StatelessWidget {
+class PlayHqHead extends StatefulWidget {
+  @override
+  _PlayHqHeadState createState() => _PlayHqHeadState();
+}
+
+class _PlayHqHeadState extends State<PlayHqHead> {
+  @override
+  void initState() {
+    super.initState();
+
+    initHive();
+  }
+
+  void initHive() async {
+    final appDocumentaryDirectory = await path_provider.getApplicationDocumentsDirectory();
+
+    Hive.init(appDocumentaryDirectory.path);
+    Hive.registerAdapter(GameDetailsAdapter());
+  }
+
+  @override
+  void dispose() {
+    Hive.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        navigatorKey: locator<NavigationService>().navigatorKey,
-        theme: ThemeData(
-          //this is what you want
-          accentColor: PRIMARY_COLOR,
-          canvasColor: Colors.transparent,
-          backgroundColor: BACKGROUND_COLOR,
-          accentColorBrightness: Brightness.light,
-          primaryTextTheme: TextTheme(
-            headline1: TextStyle(
-              fontSize: 22,
-              color: Colors.white,
-              fontFamily: Neusa,
-            ),
-            headline2: TextStyle(
-              fontSize: 18,
-              color: Colors.white.withOpacity(0.6),
-              fontFamily: CircularBook,
-            ),
-            subtitle1: TextStyle(
-              fontSize: 16,
-              color: Colors.white.withOpacity(0.6),
-              fontFamily: CircularBook,
-            ),
-            subtitle2: TextStyle(
-              fontSize: 14,
-              color: Colors.white,
-              fontFamily: CircularBook,
-            ),
-            headline3: TextStyle(
-              fontSize: 18,
-              color: Color(0xffB5BDD5).withOpacity(0.8),
-              fontFamily: CircularBold,
-            ),
-            headline6: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-              fontFamily: CircularBold,
-            ),
+      navigatorKey: locator<NavigationService>().navigatorKey,
+      theme: ThemeData(
+        //this is what you want
+        accentColor: PRIMARY_COLOR,
+        canvasColor: Colors.transparent,
+        backgroundColor: BACKGROUND_COLOR,
+        accentColorBrightness: Brightness.light,
+        primaryTextTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 22,
+            color: Colors.white,
+            fontFamily: Neusa,
+          ),
+          headline2: TextStyle(
+            fontSize: 18,
+            color: Colors.white.withOpacity(0.6),
+            fontFamily: CircularBook,
+          ),
+          subtitle1: TextStyle(
+            fontSize: 16,
+            color: Colors.white.withOpacity(0.6),
+            fontFamily: CircularBook,
+          ),
+          subtitle2: TextStyle(
+            fontSize: 14,
+            color: Colors.white,
+            fontFamily: CircularBook,
+          ),
+          headline3: TextStyle(
+            fontSize: 18,
+            color: Color(0xffB5BDD5).withOpacity(0.8),
+            fontFamily: CircularBold,
+          ),
+          headline6: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontFamily: CircularBold,
           ),
         ),
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: generateRoute,
-        initialRoute: '/');
+      ),
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: generateRoute,
+      initialRoute: '/',
+    );
   }
 }
