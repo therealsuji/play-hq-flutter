6a59c01b5c192456d3f548df4d1daac190a5136b
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-09-12 11:43:02 +0530

    fcm testing added

--- lib/view-models/splash-screen/splash-screen-impl.dart
+++ lib/view-models/splash-screen/splash-screen-impl.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:play_hq/helpers/app-secure-storage.dart';
 import 'package:play_hq/helpers/app-service-locator.dart';
 import 'package:play_hq/helpers/app-strings.dart';
@@ -19,6 +22,20 @@ class ImplSplash extends SplashScreenModel {
   @override
   void startAuthentication() async {
     var localToken = await SecureStorage.readValue('jwtToken');
+    var fcmToken = await SecureStorage.readValue('fcmToken');
+    log("strapiToken $localToken");
+    log("fcmToken $fcmToken");
+    log("firebaseToken ${await FirebaseAuth.instance.currentUser?.getIdToken()}");
+    // TESTING CODE
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
     if (FirebaseAuth.instance.currentUser != null && localToken != null) {
       navigateMainScreen();
     } else {
