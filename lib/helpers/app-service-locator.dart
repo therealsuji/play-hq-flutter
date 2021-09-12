ec0435b0bbcd3dbd9b33c143484d0818d6d8a788
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-09-12 11:43:48 +0530

    removed google auth from locator

--- lib/helpers/app-service-locator.dart
+++ lib/helpers/app-service-locator.dart
@@ -1,6 +1,5 @@
 import 'package:event_bus/event_bus.dart';
 import 'package:get_it/get_it.dart';
-import 'package:play_hq/services/auth_providers/google_auth_service.dart';
 import 'package:play_hq/services/auth_service.dart';
 import 'package:play_hq/services/nav-service.dart';
 
@@ -9,6 +8,5 @@ GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton<NavigationService>(() => NavigationService());
   locator.registerLazySingleton(() => EventBus());
-  locator.registerLazySingleton<GoogleAuthService>(() => GoogleAuthService());
   locator.registerLazySingleton<AuthService>(() => AuthService());
 }
