ed36068a80cb0dfcb08fd7c1eb0088ccc7d21fd5
MalshanP <malshan@gapstars.net>
2022-02-08 16:00:53 +0530

    Change custom body to toggle scroller and change the body according to the selected body

--- lib/screens/order_details/order_details_screen.dart
+++ lib/screens/order_details/order_details_screen.dart
@@ -4,7 +4,6 @@ import 'package:play_hq/helpers/app_colors.dart';
 import 'package:play_hq/helpers/app_enums.dart';
 import 'package:play_hq/helpers/app_screen_utils.dart';
 import 'package:play_hq/view_models/view_models.dart' show EndedOrderModel;
-import 'package:play_hq/widgets/custom_body.dart';
 import 'package:provider/provider.dart';
 
 class OrderDetailsScreen extends StatefulWidget {
@@ -19,62 +18,97 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: BACKGROUND_COLOR,
-      body: CustomBody(
-        body: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              GestureDetector(
-                child: SvgPicture.asset(
-                  'assets/icons/back.svg',
-                  color: Colors.white,
-                ),
-                onTap: () => Navigator.pop(context),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 15),
-                child: Text(
-                  'Order ID: #12345',
-                  style: Theme.of(context).primaryTextTheme.headline2!.copyWith(
-                    fontWeight: FontWeight.normal,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: ScreenUtils.getDesignHeight(20.0) + ScreenUtils.statusBarHeight
+        ),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: ScreenUtils.getDesignWidth(24.0),
+                  ),
+                  child: GestureDetector(
+                    child: SvgPicture.asset(
+                      'assets/icons/back.svg',
+                      color: Colors.white,
+                    ),
+                    onTap: () => Navigator.pop(context),
                   ),
                 ),
-              )
-            ],
-          ),
-          Container(
-            height: ScreenUtils.getDesignHeight(40),
-            margin: EdgeInsets.only(
-              top: ScreenUtils.getDesignWidth(30),
-            ),
-            decoration: BoxDecoration(
-              color: MAIN_CONTAINER_COLOR,
-              borderRadius: BorderRadius.circular(3.0),
-            ),
-            child: Selector<EndedOrderModel, EndedOrderType>(
-              selector: (_, m) => m.bodyType,
-              builder: (_, bodyType, __) {
-                return Row(
-                  children: [
-                    _toggleButton(
-                      type: EndedOrderType.ORDER_DETAILS,
-                      isSelected: EndedOrderType.ORDER_DETAILS == bodyType,
+                Container(
+                  margin: EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    'Order ID: #12345',
+                    style: Theme.of(context).primaryTextTheme.headline2!.copyWith(
+                      fontWeight: FontWeight.normal,
                     ),
-                    _toggleButton(
-                      type: EndedOrderType.BILLING,
-                      isSelected: EndedOrderType.BILLING == bodyType,
-                    ),
-                    _toggleButton(
-                      type: EndedOrderType.GAME_LIST,
-                      isSelected: EndedOrderType.GAME_LIST == bodyType,
-                    ),
-                  ],
-                );
-              }
+                  ),
+                )
+              ],
             ),
-          ),
-        ],
-      ),
+            Container(
+              height: ScreenUtils.getDesignHeight(40.0),
+              margin: EdgeInsets.only(
+                top: ScreenUtils.getDesignWidth(30.0),
+                left: ScreenUtils.getDesignWidth(24.0),
+                right: ScreenUtils.getDesignWidth(24.0),
+              ),
+              decoration: BoxDecoration(
+                color: MAIN_CONTAINER_COLOR,
+                borderRadius: BorderRadius.circular(3.0),
+              ),
+              child: Selector<EndedOrderModel, EndedOrderType>(
+                  selector: (_, m) => m.bodyType,
+                  builder: (_, bodyType, __) {
+                    return Row(
+                      children: [
+                        _toggleButton(
+                          type: EndedOrderType.ORDER_DETAILS,
+                          isSelected: EndedOrderType.ORDER_DETAILS == bodyType,
+                        ),
+                        _toggleButton(
+                          type: EndedOrderType.BILLING,
+                          isSelected: EndedOrderType.BILLING == bodyType,
+                        ),
+                        _toggleButton(
+                          type: EndedOrderType.GAME_LIST,
+                          isSelected: EndedOrderType.GAME_LIST == bodyType,
+                        ),
+                      ],
+                    );
+                  }
+              ),
+            ),
+            Expanded(
+              child: Selector<EndedOrderModel, EndedOrderType>(
+                selector: (_, m) => m.bodyType,
+                builder: (_, type, __) {
+                  switch (type) {
+                    case EndedOrderType.ORDER_DETAILS:
+                      return Container(
+                        color: Colors.red,
+                      );
+                    case EndedOrderType.BILLING:
+                      return Container(
+                        color: Colors.green,
+                      );
+                    case EndedOrderType.GAME_LIST:
+                      return Container(
+                        color: Colors.blue,
+                      );
+                    default:
+                      return Container();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 
--- lib/widgets/custom_body.dart
+++ lib/widgets/custom_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:play_hq/helpers/app_assets.dart';
 import 'package:play_hq/helpers/app_colors.dart';
 import 'package:play_hq/helpers/app_screen_utils.dart';
 
@@ -10,6 +9,7 @@ class CustomBody extends StatelessWidget {
   final double paddingBottom;
   final double paddingRight;
   final bool getStatusBar;
+  final bool isScrollable;
 
   CustomBody({
     required this.body,
@@ -18,27 +18,32 @@ class CustomBody extends StatelessWidget {
     this.paddingRight = 24.0,
     this.paddingBottom = 45.0,
     this.getStatusBar = true,
+    this.isScrollable = true,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: BACKGROUND_COLOR,
-      child: SingleChildScrollView(
-        child: Container(
-          width: ScreenUtils.bodyWidth,
-          child: Padding(
-            padding: EdgeInsets.only(
-              top: (getStatusBar ? ScreenUtils.statusBarHeight : 0.0) + ScreenUtils.getDesignHeight(paddingTop),
-              left: ScreenUtils.getDesignWidth(paddingLeft),
-              right: ScreenUtils.getDesignWidth(paddingRight),
-              bottom: ScreenUtils.getDesignWidth(paddingBottom),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: body != null && body != [] ? body : [],
-            ),
-          ),
+      child: isScrollable ? SingleChildScrollView(
+        child: _body(),
+      ) : _body(),
+    );
+  }
+
+  Widget _body() {
+    return Container(
+      width: ScreenUtils.bodyWidth,
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: (getStatusBar ? ScreenUtils.statusBarHeight : 0.0) + ScreenUtils.getDesignHeight(paddingTop),
+          left: ScreenUtils.getDesignWidth(paddingLeft),
+          right: ScreenUtils.getDesignWidth(paddingRight),
+          bottom: ScreenUtils.getDesignWidth(paddingBottom),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: body != null && body != [] ? body : [],
         ),
       ),
     );
