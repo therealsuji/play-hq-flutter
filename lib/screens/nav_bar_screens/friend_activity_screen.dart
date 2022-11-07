ce018597fd450b135adde962d5e38d725cf47758
damsara <damsara@ddilabs.ai>
2022-11-07 20:43:59 +0530

    Created UI for Friend Activity Screen

--- lib/screens/nav_bar_screens/friend_activity_screen.dart
+++ lib/screens/nav_bar_screens/friend_activity_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:play_hq/helpers/app_colors.dart';
+import 'package:play_hq/helpers/app_fonts.dart';
+import 'package:play_hq/helpers/app_screen_utils.dart';
 import 'package:play_hq/screens/nav_bar_screens/widgets/friend_activity_section.dart';
 import 'package:play_hq/widgets/custom_text_widget.dart';
+import 'package:play_hq/widgets/gradient_text_widget.dart';
+import 'package:play_hq/widgets/horizontal_scroll_widget.dart';
 import 'package:play_hq/widgets/page_view_tab_widget.dart';
+
+import '../../widgets/custom_game_widget.dart';
 
 class FriendActivity extends StatefulWidget {
   const FriendActivity({Key? key}) : super(key: key);
@@ -15,141 +22,311 @@ class _FriendActivityState extends State<FriendActivity> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: CustomTextWidget(
-              "Friend Activity",
-              style: Theme.of(context).primaryTextTheme.headline2,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-            child: PageViewTab(
-              tabsExpanded: false,
-              tabPadding: 10,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              activeTab: 0,
-              tabs: [
-                "All",
-                "Wishlist",
-                "Library",
-                Container(
-                  height: double.infinity,
-                  child: Row(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 4,
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CustomTextWidget(
-                        "Friend Requests",
-                        style: Theme.of(context).primaryTextTheme.bodyText2,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: PRIMARY_COLOR,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: CustomTextWidget(
+                          "Notifications",
+                          style: Theme.of(context).primaryTextTheme.headline2,
                         ),
-                        child: CustomTextWidget("10", style: Theme.of(context).primaryTextTheme.bodyText2),
-                      )
+                      ),
+                      _friendRequests(),
+                      Expanded(
+                        flex: 1,
+                        child: HorizontalScrollList(
+                          itemCount: 5,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                                margin: EdgeInsets.symmetric(vertical: 25),
+                                child: _friendRequestContainer());
+                          },
+                        ),
+                      ),
                     ],
                   ),
-                )
-              ],
-              onTap: ((index) {}),
+                )),
+            Expanded(
+                flex: 5,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: ScreenUtils.getDesignHeight(65),
+                        color: MAIN_CONTAINER_COLOR.withOpacity(0.6),
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 24),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Friend Activity',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontFamily: Neusa,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              Spacer(),
+                              Text(
+                                'View All',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontFamily: CircularBook,
+                                    fontWeight: FontWeight.bold,
+                                    color: PRIMARY_COLOR),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: HorizontalScrollList(
+                            itemCount: 5,
+                            axis: Axis.vertical,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                  child: friendActivityWidget());
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget friendActivityWidget(){
+    return Container(
+      decoration: BoxDecoration(
+          color: MAIN_CONTAINER_COLOR.withOpacity(0.6),
+          borderRadius: BorderRadius.circular(5)),
+      height: ScreenUtils.getDesignHeight(85),
+      width: ScreenUtils.bodyWidth,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          children: [
+            Container(
+              height: ScreenUtils.getDesignHeight(55),
+              width: ScreenUtils.getDesignWidth(55),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey
+              ),
             ),
-          ),
-          Expanded(
-            child: PageView(
-              children: [
-                ListView.separated(
-                  itemBuilder: (context, index) => ActivitySection(
-                    sectionTitle: "Today",
-                    cardDetails: [
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        rightSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Gisky ',
+                        style: TextStyle(fontWeight: FontWeight.w600 , fontSize: 16 , color: PRIMARY_COLOR , fontFamily: Neusa),
+                        children: const <TextSpan>[
+                          TextSpan(text: 'added Ghost of Tsushima to their Wishlist.', style: TextStyle(fontWeight: FontWeight.w600 , fontSize: 16 , color: Colors.white , fontFamily: Neusa)),
+                        ],
                       ),
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        buttonOneText: "Accept",
-                        buttonTwoText: "Decline",
-                        buttonOneOnTap: () {},
+                    ),
+                    Text(
+                      'Today',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: SUB_TEXT_COLOR
                       ),
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        buttonOneText: "Accept",
-                        buttonTwoText: "Decline",
-                        buttonOneOnTap: () {},
-                      ),
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        buttonOneText: "Accept",
-                        buttonTwoText: "Decline",
-                        buttonOneOnTap: () {},
-                      ),
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        buttonOneText: "Accept",
-                        buttonTwoText: "Decline",
-                        buttonOneOnTap: () {},
-                      )
-                    ],
-                  ),
-                  separatorBuilder: (context, index) => Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 20),
-                    height: 1.5,
-                    width: double.infinity,
-                  ),
-                  itemCount: 2,
+                    )
+                  ],
                 ),
-                ListView.separated(
-                  itemBuilder: (context, index) => ActivitySection(
-                    sectionTitle: "Today",
-                    cardDetails: [
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        rightSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                      ),
-                      ActivitySectionCard(
-                        text: "James Gordon wants to be friends with you",
-                        leftSideImageUrl:
-                            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80",
-                        buttonOneText: "Accept",
-                        buttonTwoText: "Decline",
-                        buttonOneOnTap: () {},
-                      )
-                    ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _friendRequestContainer() {
+    return Container(
+      height: ScreenUtils.getDesignHeight(290),
+      width: ScreenUtils.getDesignWidth(327),
+      decoration: BoxDecoration(
+          color: MAIN_CONTAINER_COLOR.withOpacity(0.6),
+          borderRadius: BorderRadius.circular(5)),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  Container(
+                    width: ScreenUtils.getDesignWidth(50),
+                    height: ScreenUtils.getDesignHeight(50),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
                   ),
-                  separatorBuilder: (context, index) => Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 20),
-                    height: 1.5,
-                    width: double.infinity,
-                  ),
-                  itemCount: 2,
-                ),
-              ],
+                  Container(
+                    margin: EdgeInsets.only(left: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'IAmDamasu ',
+                            style: TextStyle(fontWeight: FontWeight.w600 , fontSize: 14 , color: PRIMARY_COLOR , fontFamily: Neusa),
+                            children: const <TextSpan>[
+                              TextSpan(text: 'has sent you a friend request.', style: TextStyle(fontWeight: FontWeight.w600 , fontSize: 14 , color: Colors.white , fontFamily: Neusa)),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: '02 ',
+                            style: TextStyle(fontWeight: FontWeight.w600 , fontSize: 14 , color: PRIMARY_COLOR , fontFamily: CircularBook),
+                            children: const <TextSpan>[
+                              TextSpan(text: 'Wishlist Games in your Library', style: TextStyle(fontWeight: FontWeight.w600 , fontSize: 14 , color: SUB_TEXT_COLOR , fontFamily: CircularBook)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              height: ScreenUtils.getDesignHeight(40),
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 15),
+                        decoration: BoxDecoration(
+                            gradient: ALERT_GRADIENT,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Center(
+                            child: Text(
+                          'Decline',
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            gradient: GREEN_GRADIENT),
+                        child: Center(
+                            child: Text(
+                          'Accept',
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )),
+                      )),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _friendRequests() {
+    return Container(
+      margin: EdgeInsets.only(top: 30),
+      height: ScreenUtils.getDesignHeight(65),
+      color: MAIN_CONTAINER_COLOR.withOpacity(0.6),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Row(
+          children: [
+            Container(
+              height: ScreenUtils.getDesignHeight(45),
+              width: ScreenUtils.getDesignWidth(45),
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Friend Requests',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: Neusa,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    'Accept or Reject Requests',
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontFamily: CircularBook,
+                        fontWeight: FontWeight.w500,
+                        color: SUB_TEXT_COLOR),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Text(
+              'View All',
+              style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: CircularBook,
+                  fontWeight: FontWeight.w500,
+                  color: PRIMARY_COLOR),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 15),
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                  gradient: PRIMARY_GRADIENT, shape: BoxShape.circle),
+              child: Center(
+                  child: Text(
+                '27',
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: CircularBook,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/horizontal_scroll_widget.dart
+++ lib/widgets/horizontal_scroll_widget.dart
@@ -4,6 +4,7 @@ import 'package:play_hq/helpers/app_screen_utils.dart';
 class HorizontalScrollList extends StatelessWidget {
   final IndexedWidgetBuilder itemBuilder;
   final int itemCount;
+  final Axis? axis;
   final Widget? emptyListWidget;
   final String emptyListText;
 
@@ -11,6 +12,7 @@ class HorizontalScrollList extends StatelessWidget {
     Key? key,
     required this.itemCount,
     required this.itemBuilder,
+    this.axis = Axis.horizontal,
     this.emptyListWidget,
     this.emptyListText = "No games available yet",
   }) : super(key: key);
@@ -20,10 +22,11 @@ class HorizontalScrollList extends StatelessWidget {
       child: itemCount > 0
           ? ListView.separated(
               separatorBuilder: (_, __) => SizedBox(
-                width: ScreenUtils.getDesignHeight(15.0),
+                width: axis == Axis.horizontal ? ScreenUtils.getDesignWidth(15.0) : 0,
+                height: axis == Axis.horizontal ? 0 : ScreenUtils.getDesignHeight(15),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              scrollDirection: Axis.horizontal,
+              padding: axis == Axis.horizontal ? EdgeInsets.symmetric(horizontal: 24) : EdgeInsets.symmetric(vertical: 24 , horizontal: 24),
+              scrollDirection: axis ?? Axis.horizontal,
               itemBuilder: itemBuilder,
               itemCount: itemCount,
             )
